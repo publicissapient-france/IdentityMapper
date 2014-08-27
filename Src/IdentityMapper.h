61561a6cd80152957a3cdd07fd8eef74a696a704
pjechris <jcpastant@xebia.fr>
2014-08-27 16:48:25 +0200

    project setup

---
+++ Src/IdentityMapper.h
@@ -0,0 +1,13 @@
+//
+//  IdentityMapper.h
+//  IdentityMapper
+//
+//  Created by JC on 27/08/14.
+//  Copyright (c) 2014 fr.milkshake. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IdentityMapper : NSObject
+
+@end
---
+++ Src/IdentityMapper.m
@@ -0,0 +1,13 @@
+//
+//  IdentityMapper.m
+//  IdentityMapper
+//
+//  Created by JC on 27/08/14.
+//  Copyright (c) 2014 fr.milkshake. All rights reserved.
+//
+
+#import "IdentityMapper.h"
+
+@implementation IdentityMapper
+
+@end
